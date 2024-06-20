41197faad0a0826af5b15ef52c8638a8c5fec56a
Marcelino <marcelino85albuquerque@gmail.com>
2024-06-20 16:54:55 -0300

    Incrementando um frontend com react

---
+++ backend/src/server.js
@@ -0,0 +1,8 @@
+import app from "./app.js"
+
+const PORT = process.env.PORT || 3006
+
+app.listen(PORT,()=>{
+    console.log(`Servidor rodando com sucesso na porta http://localhost:${PORT}`)
+})
+""
