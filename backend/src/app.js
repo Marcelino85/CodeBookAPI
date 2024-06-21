b32a19f1ed822783f3161edafdb079071ba7f6d0
Marcelino <marcelino85albuquerque@gmail.com>
2024-06-21 01:02:03 -0300

    realizando a integração do back com o front e testando

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import routes from './routes.js';
+import cors from 'cors';
 
 const app = express()
 //indicar para o express ler
 app.use(express.json())
+
+app.use(cors())
 
 //user router depois do app.user(express.json())
 app.use(routes)
--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
+import { useEffect } from 'react'
+import {useState} from 'react'
+import axios from 'axios'
 
 const Books = () => {
+  
+    const [livro,setLivro] = useState([])
+
+    useEffect(()=>{
+      const fecthAllBooks = async ()=>{
+        try{
+          const res = await axios.get("http://localhost:3006/livros")
+          console.log(res)
+        }catch(err){
+          console.log(err)
+        }
+      }
+
+      fecthAllBooks()
+
+    },[])
+  
   return (
     <div>
       Books
@@ -8,4 +28,4 @@ const Books = () => {
   )
 }
 
-export default Books
+export default Books;
