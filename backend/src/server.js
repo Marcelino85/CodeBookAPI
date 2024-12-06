f3128c9ab391b940d0ca3b61ab0172d981fda453
Marcelino <marcelino85albuquerque@gmail.com>
2024-12-06 12:37:05 -0300

    feat(main): Tirando a redundancia da Porta do express no service

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,8 @@ import app from "./app.js"
 import dotenv from "dotenv";
 
 dotenv.config(); // Carrega as variáveis do .env
-const PORT = process.env.APP_PORT || 3000; // Porta do servidor HTTP
+
+const PORT = process.env.APP_PORT // Porta do servidor HTTP
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando com sucesso na porta http://localhost:${PORT}`);
